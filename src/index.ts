1921ac4310078c86240eb3f19c7c1e4b2e3fac34
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-11-18 20:06:33 +0000

    Get data from Xata

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
-import express, {Express, Request, Response} from 'express'
-// @ts-ignore
+import express from 'express'
 import dotenv from 'dotenv'
+import {getXataClient} from "./xata";
 
-const app: Express = express()
+dotenv.config()
+
+const app = express()
 const port = process.env.PORT || 8080;
 
+const xata = getXataClient();
 
-app.get('/status', (req:Request, res: Response)=>{
+app.get('/status', (req, res) => {
     res.json({
         status: 'Up'
     })
+})
+
+app.get('/api/jobs', async (req, res) => {
+    const jobs = await xata.db.job.getAll()
+    res.json({jobs})
 })
 
 app.listen(port, () => {
